c0bb0c0eae69f2c82d651a1e888bd0833369300a
David Urman <david.urman@nexergroup.com>
2022-12-13 17:07:20 +0100

    Changes by anton

--- src/main/java/com/groupthree/controller/MovieController.java
+++ src/main/java/com/groupthree/controller/MovieController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import java.util.List;
 
 @Controller
 @RequestMapping("/website/movies")
